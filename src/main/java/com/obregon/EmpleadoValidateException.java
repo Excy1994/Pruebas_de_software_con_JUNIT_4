ecd6d17685a769faabc78f59f66ccc399b75ceba
Excy1994 <obregondoribel@gmail.com>
2021-11-05 02:53:01 -0600

    EmpleadoValidateException

--- src/main/java/com/obregon/EmpleadoValidateException.java
+++ src/main/java/com/obregon/EmpleadoValidateException.java
@@ -1,2 +1,24 @@
 package com.obregon;
 
+public class EmpleadoValidateException extends Exception {
+    private int CodigoError;
+
+    public EmpleadoValidateException(int codigoError){
+        super();
+        this.CodigoError = codigoError;
+    }
+
+    @Override
+    public String getMessage(){
+        String mensaje = "";
+
+        switch (CodigoError){
+            case 245:
+                mensaje = "El campo de tipo de vendedor no existe";
+                break;
+        }
+        return mensaje;
+    }
+
+}
+
